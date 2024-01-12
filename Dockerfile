f1e3cf3e158f1a1dc10d20c96c42e609409eb69e
Monokim01 <mono.kim@goorm.io>
2024-01-12 01:00:35 +0000

    test

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:16
+WORKDIR /usr/src/app
+COPY ./ ./
+RUN npm i
+EXPOSE 3000
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const socketIo = require('socket.io');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+    });
+  } else {
+    res.end('Hello World!');
+  }
+});
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('buttonClicked', (message) => {
+    console.log(message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Listening on *:3000');
+});
