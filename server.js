42ca47de593de4c74981d614678bf2941ac1ff1c
hostdog01 <hostdog01@naver.com>
2024-01-12 01:27:22 +0000

    package please

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ const http = require('http');
 const socketIo = require('socket.io');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const multer = require('multer');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading index.html');
-      }
-
-      res.writeHead(200);
-      res.end(data);
-    });
-  } else {
-    res.end('Hello World!');
+// Multer 설정
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')  // 파일이 저장될 경로입니다.
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
   }
 });
+const upload = multer({ storage: storage });
 
+// 서버 및 Socket.IO 설정
+const server = http.createServer(app);
 const io = socketIo(server);
 
 io.on('connection', (socket) => {
@@ -26,13 +26,21 @@ io.on('connection', (socket) => {
 
   socket.on('buttonClicked', (message) => {
     console.log(message);
-    socket.emit("hello");
+    socket.emit('hello');
   });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
+
+// 업로드 라우트
+app.post('/upload', upload.single('file'), (req, res) => {
+  console.log('File uploaded');
+  res.send('File uploaded successfully!');
+});
+
+app.use(express.static(path.join(__dirname)));
 
 server.listen(3000, () => {
   console.log('Listening on *:3000');
